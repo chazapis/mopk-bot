27f81ba79c0fcdb9025937cd6bc43069c663fd59
Antony Chazapis <chazapis@ics.forth.gr>
2024-04-05 22:30:38 +0300

    Fortune teller

---
+++ README.md
@@ -0,0 +1,11 @@
+# MOPK Bot
+
+A simple Discord bot, based on [this](https://realpython.com/how-to-make-a-discord-bot-python/) excellent tutorial.
+
+To run:
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python bot.py
+```
---
+++ bot.py
@@ -0,0 +1,33 @@
+import os
+
+import discord
+
+from dotenv import load_dotenv
+from fortune import fortune
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'hit me!':
+        response = fortune()
+        await message.channel.send(response)
+
+client.run(TOKEN)
